fda22f2021dbc05bc20adef4ca3aba5000a51779
Sneha <snehadoshi.ap@gmail.com>
2025-01-21 17:38:37 +0530

    Content Pages

--- resources/views/pages/refundPolicy.blade.php
+++ resources/views/pages/refundPolicy.blade.php
@@ -27,31 +27,107 @@
     <div class="container">
         <h3 class="text-slate-900 text-4xl font-hubot font-semibold mb-3 text-32px-mob">Refund Policy</h3>
         <div class="mb-4">
-            <h4 class="text-slate-900 text-2xl mt-4 font-hubot font-semibold">Hassle-Free Returns</h4>
-            <p class="text-gray-500 text-lg font-inter-regular mb-0">We want you to be completely satisfied with your Skootz purchase.</p>
+          
+            <p class="text-gray-500 text-lg font-inter-regular mb-0">At Skootz, your satisfaction is our priority. If you’re unsatisfied with your purchase, we’re here to make it right with a hassle-free return and refund process.
+            </p>
         </div>
         <div class="mb-4">
-            <h4 class="text-slate-900 text-2xl font-hubot font-semibold">Eligibility for Refunds</h4>
+            <h4 class="text-slate-900 text-2xl font-hubot font-semibold">Eligibility for Returns and Refunds
+            </h4>
+            <p class="text-gray-500 text-lg font-inter-regular mb-0">To qualify for a return or refund, the following conditions must be met:</p>
             <ul>
-                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Items must be returned within 30 days of delivery.</li>
-                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Products must be in their original condition, unused, and with all packaging intact.</li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Return Timeframe:</b> Items must be returned within <b>30 days of delivery.</b>
+                </li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Condition of Items:</b> Products must be in their <b>original condition</b>, unused, and with all packaging intact.
+                </li>
+                <li><b>Eligible Circumstances:</b>
+                    <ul>
+                        <li>Received a defective or damaged product.</li>
+                        <li>Changed your mind (product must remain unused and in original condition).</li>
+                    </ul>
+                    </li>
+            </ul>
+        </div>
+        <div class="mb-4">
+            <h4 class="text-slate-900 text-2xl font-hubot font-semibold">How to Initiate a Return
+            </h4>
+            <p class="text-gray-500 text-lg font-inter-regular mb-0">To start a return, please follow these simple steps:</p>
+            <ul>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Contact Us: Email us at <a href="mailto:runmaxlimited@gmail.com" class="font-inter-semibold text-decoration-none text-slate-900">runmaxlimited@gmail.com</a> with the following details:
+                    <ul>
+                        <li>Your order number.</li>
+                        <li>Reason for return.</li>
+                        <li>Supporting images (if applicable, e.g., for damaged or defective items).</li>
+                    </ul>
+                </li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Confirmation:</b> Our support team will review your request and provide you with return instructions.
+                </li>
             </ul>
         </div>
         <div class="mb-4">
             <h4 class="text-slate-900 text-2xl font-hubot font-semibold">Refund Process</h4>
+            <p class="text-gray-500 text-lg font-inter-regular mb-0">Once we receive and inspect the returned item:</p>
             <ul>
-                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Contact us at <a href="mailto:runmaxlimited@gmail.com" class="font-inter-semibold text-decoration-none text-slate-900">runmaxlimited@gmail.com</a> to initiate the return.</li>
-                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Once we receive and inspect the item, a refund will be processed within 7 business days.</li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Inspection Timeframe:</b> We will inspect the item within <b>2 business days</b> of receipt.</li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Refund Approval:</b> the item meets the eligibility criteria, your refund will be approved.</li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Refund Timeline:</b> Refunds will be processed within <b>7 business days</b> after approval.</li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Refund Method: </b> will be credited to your original payment method.</li>
             </ul>
+          
         </div>
+
         <div class="mb-4">
             <h4 class="text-slate-900 text-2xl font-hubot font-semibold">Non-Refundable Items</h4>
+            <p class="text-gray-500 text-lg font-inter-regular mb-0">We are unable to accept returns or issue refunds for:</p>
+
             <ul>
-                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Customized products or items damaged by misuse.</li>
-                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Sale items, unless proven defective.</li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Customized or personalized products.</li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Items damaged due to misuse or improper handling.
+                </li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Sale or discounted items, unless proven defective.
+                </li>
             </ul>
-            <p class="text-gray-500 text-lg font-inter-regular mb-0">For full details, visit our website or contact our support team.</p>
+          
         </div>
+
+        <div class="mb-4">
+            <h4 class="text-slate-900 text-2xl font-hubot font-semibold">Important Notes
+            </h4>
+           
+            <ul>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Customers are responsible for return shipping costs unless the return is due to our error (e.g., wrong or defective item sent).
+                </li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">We recommend using a trackable shipping method to ensure safe return delivery.
+                </li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Based on your payment provider's policies, refunds may take additional time to reflect in your account.
+
+                </li>
+            </ul>
+          
+        </div>
+
+        <div class="mb-4">
+            <h4 class="text-slate-900 text-2xl font-hubot font-semibold">Contact Us for Support
+
+            </h4>
+            <p class="text-gray-500 text-lg font-inter-regular mb-0">For any questions or assistance regarding your return, feel free to reach out:</p>
+
+            <ul>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Email: </b> <a href="mailto:runmaxlimited@gmail.com" class="font-inter-semibold text-decoration-none text-slate-900">runmaxlimited@gmail.com</a>
+                </li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Phone:</b> +44 7418356616
+
+                </li>
+              
+            </ul>
+            <p class="text-gray-500 text-lg font-inter-regular mb-0">Our team is available during support hours to assist you promptly.</p>
+            <p class="text-gray-500 text-lg font-inter-regular mb-0">For full details, please visit our <a href="{{ route( 'terms-conditions' ) }}" class="font-inter-semibold text-decoration-none text-slate-900">Terms and Conditions</a> or <a href="{{ route('shipping-policy') }}" class="font-inter-semibold text-decoration-none text-slate-900">Shipping Policy.</a>
+            </p>
+            <p class="text-gray-500 text-lg font-inter-regular mb-0">Thank you for choosing Skootz. We’re here to ensure your satisfaction with every purchase!
+            </p>
+          
+        </div>
+
     </div>
 </section>
 
--- resources/views/pages/shippingPolicy.blade.php
+++ resources/views/pages/shippingPolicy.blade.php
@@ -27,24 +27,67 @@
     <div class="container">
         <h3 class="text-slate-900 text-4xl font-hubot font-semibold mb-3 text-32px-mob">Shipping Policy</h3>
         <div>
-            <h4 class="text-slate-900 text-2xl mt-4 font-hubot font-semibold">Delivery Options</h4>
+            <h4 class="text-slate-900 text-2xl font-hubot font-semibold">Delivery Options</h4>
             <ul>
-                <li class="text-gray-500 text-lg font-inter-regular list-disc"><span class="text-slate-900 font-inter-semibold">Standard Delivery:</span> 3–5 business days.</li>
-                <li class="text-gray-500 text-lg font-inter-regular list-disc"><span class="text-slate-900 font-inter-semibold">Express Delivery:</span> 1–2 business days (additional fees apply).</li>
-                <li class="text-gray-500 text-lg font-inter-regular list-disc"><span class="text-slate-900 font-inter-semibold">Cash on Delivery (COD):</span> Available across most UK regions.</li>
+                <li class="text-gray-500 text-lg font-inter-regular list-disc"><span class="text-slate-500 font-inter-semibold">Standard Delivery:</span>  Delivered within 3–5 business days</li>
+                <li class="text-gray-500 text-lg font-inter-regular list-disc"><span class="text-slate-500 font-inter-semibold">Express Delivery:</span> Delivered within 1–2 business days (additional fees apply)</li>
+                <li class="text-gray-500 text-lg font-inter-regular list-disc"><span class="text-slate-500 font-inter-semibold">Cash on Delivery (COD):</span> Available across most UK regions. Check during checkout for eligibility.
+                </li>
             </ul>
         </div>
         <div>
-            <h4 class="text-slate-900 text-2xl mt-4 font-hubot font-semibold">Shipping Charges</h4>
+            <h4 class="text-slate-900 text-2xl font-hubot font-semibold">Processing Time
+            </h4>
             <ul>
-                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Free delivery on orders over £100.</li>
-                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">A small fee applies for orders below £100 or COD services.</li>
+                <li class="text-gray-500 text-lg font-inter-regular list-disc">Orders are processed within 1 business day after payment confirmation. Orders placed on weekends or public holidays will be processed the next business day.</li>
+                
+            </ul>
+        </div>
+        <div>
+            <h4 class="text-slate-900 text-2xl font-hubot font-semibold">Shipping Charges</h4>
+            <ul>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Free Delivery:</b> Available for orders over £100.
+                </li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Standard Delivery Fee:</b> A small fee applies for orders below £100 or when using COD services.
+                </li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Additional fees for Express Delivery will be displayed at checkout.
+                </li>
             </ul>
         </div>
         <div class="mb-4">
             <h4 class="text-slate-900 text-2xl mt-sm-5 mt-4 font-hubot font-semibold">Delivery Areas</h4>
-            <p class="text-gray-500 text-lg font-inter-regular mb-0 mt-3">We currently deliver across the UK. For COD availability in your region, check during checkout or contact us.</p>  
+            <ul><li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">We currently deliver across the UK.</li><li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"> For COD availability in your region, check during checkout or contact us.</li>  </ul>
         </div>
+        <div class="mb-4">
+            <h4 class="text-slate-900 text-2xl mt-sm-5 mt-4 font-hubot font-semibold">Tracking Information</h4>
+           <ul> <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Once your order is shipped, you will receive a tracking number via email to monitor your delivery status.
+        </li></ul>
+        </div>
+
+        <div class="mb-4">
+            <h4 class="text-slate-900 text-2xl mt-sm-5 mt-4 font-hubot font-semibold">Potential Delays
+            </h4>
+           <ul> <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">Please note that delivery timelines may be extended during public holidays or periods of high demand.
+            </li><li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc">In the event of delays, our team will inform you via email or phone.</li></ul>
+        </div>
+
+        <div class="mb-4">
+            <h4 class="text-slate-900 text-2xl font-hubot font-semibold">Contact Us for Support
+
+            </h4>
+            <p class="text-gray-500 text-lg font-inter-regular mb-0">For any questions or assistance regarding your return, feel free to reach out:</p>
+
+            <ul>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Email: </b> <a href="mailto:runmaxlimited@gmail.com" class="font-inter-semibold text-decoration-none text-slate-900">runmaxlimited@gmail.com</a>
+                </li>
+                <li class="text-gray-500 text-lg font-inter-regular mb-0 list-disc"><b>Phone:</b> +44 7418356616
+
+                </li>
+              
+            </ul>
+            <p class="text-gray-500 text-lg font-inter-regular mb-0">Our team is available during support hours to assist you promptly.</p>
+        </div>
+
     </div>
 </section>
 
