9b877312641c9408377456fc89711846682fe42f
Pratik Sodha <pratiksodha.ap@gmail.com>
2025-01-22 11:07:38 +0530

    - SKU field display in product details page

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -137,6 +137,10 @@
                         <h6 class="min-w-100 mb-0 text-gray-500 font-inter-regular">Availability</h6>
                         <h6 class="mb-0 text-slate-900 font-inter-semibold">In Stock</h6>
                     </div>
+                    <div class="d-flex align-items-center my-3"> 
+                        <h6 class="min-w-100 mb-0 text-gray-500 font-inter-regular">SKU</h6>
+                        <h6 class="mb-0 text-slate-900 font-inter-semibold">{{ $product->sku }}</h6>
+                    </div>
                     <div class="d-flex align-items-center mb-3"> 
                         <h6 class="min-w-100 mb-0 text-gray-500 font-inter-regular">Condition</h6>
                         <h6 class="mb-0 text-slate-900 font-inter-semibold">NEW</h6>
