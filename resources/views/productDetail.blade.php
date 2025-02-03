a3dad620e07b180ed6acca18e9738d918c483daa
Sneha <snehadoshi.ap@gmail.com>
2025-02-03 18:44:30 +0530

    product detail conversion tag

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.master')
 @section('title') {{$product->slider_title}} | Skootz  @endsection
 @section('description') {{$product->slider_content}} @endsection
+@section('conversion')  
+<script>
+    gtag('event', 'conversion', {'send_to': 'AW-16832855332/BGUeCOmh4ZcaEKT6w9o-'});
+  </script>
+@endsection
 @section('content')
 
 @php
