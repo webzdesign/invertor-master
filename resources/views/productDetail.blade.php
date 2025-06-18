de6efc1291752d54bc2f8d0b23c57827bf8109bf
jay <baraiyaj.ap@gmail.com>
2025-06-18 18:03:29 +0530

    fix images issue

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -188,47 +188,51 @@
         </div>
     </div>
 </section>
-@php
-    $last_img= 0;
-    $lastimgs = $product->images->toArray();
-    if(count($lastimgs) > 1) {
-        $last_img = end($lastimgs)['name'];
-    }
-    $yt_url = !empty($product->youtube_video_url) ? $product->youtube_video_url : null;
 
-@endphp
 @if( !empty($product->images) )
+    @php
+        $images = collect($product->images);
+
+        $last_img = null;
+        $yt_url = $product->youtube_video_url;
+
+        if (!empty($yt_url) && $images->isNotEmpty()) {
+            $last_img_obj = $images->pop();
+            $last_img = $last_img_obj->name ?? null;
+        }
+    
+    @endphp
     <section class="why-choose-product bg-gray-50">
         <h2 class="text-6xl text-4xl-mob font-bebas text-slate-900 mb-sm-5 mb-3 px-3 px-sm-0">{{__('Why Choose the')}} {{ $product->name }}?</h2>
         <div class="row m-0">
-            @foreach($product->images as $image)
-                @if ($image->name != $last_img)
-                    <div class="col-xl-3 col-lg-4 col-md-6 mb-4">
-                        <img class="sz_banner_img" src="{{ env('APP_Image_URL') . 'storage/product-images/' . $image->name }}" alt="{{ $image->name }}">
-                    </div>
-                @else
-                    <div class="col-12 mb-4">
-                        <div class="position-relative">
-                            <img class="sz_banner_img" src="{{ env('APP_Image_URL') . 'storage/product-images/' . $last_img }}" alt="{{ $last_img }}">
-                            @if (!empty($yt_url))
-                                <div class="position-absolute top-50 left-50 translate-middle cursor-pointer sz_youtube_video_btn" data-youtubeUrl="{{ $yt_url }}">
-                                    <svg class="icon-play" width="116" height="115" viewBox="0 0 116 115" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g clip-path="url(#clip0_67_17706)">
-                                        <rect x="0.523438" y="0.337891" width="115.008" height="114.662" rx="57.3309" fill="black" fill-opacity="0.25"></rect>
-                                        <path d="M58.0273 97.668C35.9353 97.668 18.0273 79.76 18.0273 57.668C18.0273 35.576 35.9353 17.668 58.0273 17.668C80.1193 17.668 98.0273 35.576 98.0273 57.668C98.0273 79.76 80.1193 97.668 58.0273 97.668ZM52.5153 43.328C52.2746 43.1674 51.9948 43.075 51.7058 43.0609C51.4167 43.0467 51.1292 43.1111 50.8739 43.2474C50.6186 43.3837 50.4051 43.5867 50.256 43.8347C50.1069 44.0827 50.0279 44.3666 50.0273 44.656V70.68C50.0279 70.9694 50.1069 71.2532 50.256 71.5012C50.4051 71.7493 50.6186 71.9523 50.8739 72.0885C51.1292 72.2248 51.4167 72.2893 51.7058 72.2751C51.9948 72.2609 52.2746 72.1686 52.5153 72.008L72.0313 59C72.2508 58.8539 72.4308 58.6559 72.5553 58.4235C72.6798 58.1911 72.7449 57.9316 72.7449 57.668C72.7449 57.4043 72.6798 57.1448 72.5553 56.9124C72.4308 56.68 72.2508 56.482 72.0313 56.336L52.5113 43.328H52.5153Z" fill="#FEFEFE"></path>
-                                        </g>
-                                        <defs>
-                                        <clipPath id="clip0_67_17706">
-                                        <rect x="0.523438" y="0.337891" width="115.008" height="114.662" rx="57.3309" fill="white"></rect>
-                                        </clipPath>
-                                        </defs>
-                                    </svg>
-                                </div>
-                            @endif
-                        </div>
-                    </div>
-                @endif
+            @foreach($images as $image)
+                <div class="col-xl-3 col-lg-4 col-md-6 mb-4">
+                    <img class="sz_banner_img" src="{{ env('APP_Image_URL') . 'storage/product-images/' . $image->name }}" alt="{{ $image->name }}">
+                </div>
             @endforeach
+            @if (!empty($yt_url) && !empty($last_img))
+                <div class="col-12 mb-4">
+                    <div class="position-relative">
+                        <img class="sz_banner_img" src="{{ env('APP_Image_URL') . 'storage/product-images/' . $last_img }}" alt="{{ $last_img }}">
+                        @if (!empty($yt_url))
+                            <div class="position-absolute top-50 left-50 translate-middle cursor-pointer sz_youtube_video_btn" data-youtubeUrl="{{ $yt_url }}">
+                                <svg class="icon-play" width="116" height="115" viewBox="0 0 116 115" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_67_17706)">
+                                    <rect x="0.523438" y="0.337891" width="115.008" height="114.662" rx="57.3309" fill="black" fill-opacity="0.25"></rect>
+                                    <path d="M58.0273 97.668C35.9353 97.668 18.0273 79.76 18.0273 57.668C18.0273 35.576 35.9353 17.668 58.0273 17.668C80.1193 17.668 98.0273 35.576 98.0273 57.668C98.0273 79.76 80.1193 97.668 58.0273 97.668ZM52.5153 43.328C52.2746 43.1674 51.9948 43.075 51.7058 43.0609C51.4167 43.0467 51.1292 43.1111 50.8739 43.2474C50.6186 43.3837 50.4051 43.5867 50.256 43.8347C50.1069 44.0827 50.0279 44.3666 50.0273 44.656V70.68C50.0279 70.9694 50.1069 71.2532 50.256 71.5012C50.4051 71.7493 50.6186 71.9523 50.8739 72.0885C51.1292 72.2248 51.4167 72.2893 51.7058 72.2751C51.9948 72.2609 52.2746 72.1686 52.5153 72.008L72.0313 59C72.2508 58.8539 72.4308 58.6559 72.5553 58.4235C72.6798 58.1911 72.7449 57.9316 72.7449 57.668C72.7449 57.4043 72.6798 57.1448 72.5553 56.9124C72.4308 56.68 72.2508 56.482 72.0313 56.336L52.5113 43.328H52.5153Z" fill="#FEFEFE"></path>
+                                    </g>
+                                    <defs>
+                                    <clipPath id="clip0_67_17706">
+                                    <rect x="0.523438" y="0.337891" width="115.008" height="114.662" rx="57.3309" fill="white"></rect>
+                                    </clipPath>
+                                    </defs>
+                                </svg>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            @endif
+        </div>
     </section>
 @endif
 
@@ -517,5 +521,4 @@
 
 </script>
 @endsection
-
 
